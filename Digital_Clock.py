38fcc52c71b9909ead0af17adfbf9fc829fe3850
MohammedYassin <M0hammed.Y4ssin@gmail.com>
2025-03-18 17:29:38 +0200

    first project digital clock

---
+++ Digital_Clock.py
@@ -0,0 +1,13 @@
+import time
+
+# countdown digital clock 
+my_time = int(input("Enter the time in seconds: "))
+
+for t in range(my_time,0, -1):
+    seconds = t % 60
+    minutes = int((t / 60)) % 60
+    hours = int((t / 60) / 60)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+
+print("Time's UP !") 
